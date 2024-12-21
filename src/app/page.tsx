6ebf853ec44fd1ab1685b2c4fbea07a8de5405c5
jktham-sf <jonas.tham@gmx.ch>
2024-12-21 23:54:12 +0100

    fix image paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import nextConfig from "../../next.config";
 
 export default function Home() {
   return (
@@ -7,7 +8,7 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src={`${nextConfig.basePath}/next.svg`}
           alt="Next.js logo"
           width={180}
           height={38}
@@ -29,7 +30,7 @@ export default function Home() {
           >
             <Image
               className={styles.logo}
-              src="/vercel.svg"
+              src={`${nextConfig.basePath}/vercel.svg`}
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -54,7 +55,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src={`${nextConfig.basePath}/file.svg`}
             alt="File icon"
             width={16}
             height={16}
@@ -68,7 +69,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src={`${nextConfig.basePath}/window.svg`}
             alt="Window icon"
             width={16}
             height={16}
@@ -82,7 +83,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/globe.svg"
+            src={`${nextConfig.basePath}/globe.svg`}
             alt="Globe icon"
             width={16}
             height={16}
