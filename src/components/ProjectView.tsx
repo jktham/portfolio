f8180193605be8a050d41a786528f5d1bb1f8b4e
jktham-pc <jonas.tham@gmx.ch>
2024-12-23 23:50:12 +0100

    info box layout

--- src/components/ProjectView.tsx
+++ src/components/ProjectView.tsx
@@ -2,39 +2,41 @@ import {Project} from "@/lib/projects";
 import styles from "@/styles/ProjectView.module.css"
 import nextConfig from "../../next.config";
 import {Swiper, SwiperSlide} from "swiper/react";
-import {Navigation, Pagination, Autoplay} from "swiper/modules";
+import {Navigation, Pagination, Autoplay, Mousewheel} from "swiper/modules";
 import "swiper/css";
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 export default function ProjectView({project, index}: {project: Project; index: number}) {
 	return (
-		<div className={index % 2 == 0 ? styles.projectView : [styles.projectView, styles.reverse].join(" ")}>
+		<div className={index % 2 == 0 ? `${styles.projectView} ${styles.reverse}` : styles.projectView}>
 			<div className={styles.info}>
-				<div className={styles.name}>{project.name}</div>
-				<div className={styles.subtitle}>
-					<div className={styles.year}>{project.year}</div>
-					<div className={styles.status}>{project.status}</div>
-				</div>
-				<div className={styles.tools}>
+				{project.name && <div className={styles.name}>{project.name}</div>}
+				{(project.year || project.status) && <div className={styles.subtitle}>
+					{project.year && <div className={styles.year}>{project.year}</div>}
+					{project.status && <div className={styles.status}>{project.status}</div>}
+				</div>}
+				{(project.tools.length > 0) && <div className={styles.tools}>
 					{project.tools.map((tool, i) => <div className={styles.tool} key={i}>{tool}</div>)}
-				</div>
-				<div className={styles.description}>{project.description}</div>
-				<div className={styles.contributors}>
-					{project.contributors.map((contributor, i) => <a className={styles.contributor} href={contributor.link} key={i}>{contributor.name}</a>)}
-				</div>
-				<div className={styles.links}>
-					<a className={styles.repo} href={project.repo}>Repo</a>
-					<a className={styles.live} href={project.live}>Live</a>
-				</div>
+				</div>}
+				{project.description && <div className={styles.description}>{project.description}</div>}
+				{(project.contributors.length > 0) && <div className={styles.contributors}>
+					{project.contributors.map((contributor, i) => <a className={styles.contributor} href={contributor.link} key={i}><span className="material-symbols-outlined">person</span>{contributor.name}</a>)}
+				</div>}
+				{(project.repo || project.live) && <div className={styles.links}>
+					{project.repo && <a className={styles.repo} href={project.repo}><span className="material-symbols-outlined">Code</span>Repo</a>}
+					{project.live && <a className={styles.live} href={project.live}><span className="material-symbols-outlined">Public</span>Live</a>}
+				</div>}
 			</div>
 			<div className={styles.images}>
 				<Swiper 
-					modules={[Navigation, Pagination, Autoplay]} 
+					modules={[Navigation, Pagination, Autoplay, Mousewheel]} 
 					navigation={false} 
 					pagination={{clickable: true}} 
 					loop={true} 
-					autoplay={{delay: 4000, disableOnInteraction: false}}
+					autoplay={{delay: 4000, disableOnInteraction: false}} 
+					spaceBetween={20} 
+					mousewheel={true} 
 				>
 					{project.images.map((image, i) => 
 						<SwiperSlide key={i}>
