67afa0e8da8d814819a21d4e7402cf3c65be01c0
jktham <jonas.tham@gmx.ch>
2025-08-13 22:57:23 +0200

    update projects

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -5,7 +5,7 @@ export type Project = {
 	status: string;
 	statusIcon: string;
 	repo: string;
-	live: string;
+	live?: string;
 	tools: string[];
 	images: string[];
 	contributors: {
@@ -56,12 +56,12 @@ export const projects: Project[] = [{
 		}],
 	}, {
 		name: "Eudox",
-		description: "Engine for fun shaders and stuff. Supports custom per object shader stages and postprocessing effects.",
+		description: "Engine for fun shaders and stuff. Supports various per object shader stages and postprocessing effects.",
 		year: "2023-2024",
 		status: "In Progress",
 		statusIcon: "more_horiz",
 		repo: "https://github.com/jktham/eudox",
-		live: "",
+		live: undefined,
 		tools: ["C++", "GLSL", "OpenGL"],
 		images: ["eudox/1.jpg", "eudox/2.jpg", "eudox/3.jpg", "eudox/4.jpg", "eudox/5.jpg", "eudox/6.jpg", "eudox/7.jpg", "eudox/8.jpg"],
 		contributors: [{
@@ -69,8 +69,22 @@ export const projects: Project[] = [{
 			link: "https://github.com/jktham",
 		}],
 	}, {
+		name: "Ambi",
+		description: "Yet another engine, this time with WebGPU^^. Trying out more cohesive scenes and visual styles.",
+		year: "2025",
+		status: "In Progress",
+		statusIcon: "more_horiz",
+		repo: "https://github.com/jktham/ambi",
+		live: "https://ambi.jktham.dev",
+		tools: ["TypeScript", "WGSL", "WebGPU"],
+		images: ["ambi/1.png", "ambi/2.png"],
+		contributors: [{
+			name: "jktham",
+			link: "https://github.com/jktham",
+		}],
+	}, {
 		name: "Exo",
-		description: "Low res 3D spaceflight sim inspired by Elite (1984). Draws directly to framebuffer, runs in browser via wasm.",
+		description: "Low res 3D spaceflight sim inspired by Elite (1984). Rasterizes polygons directly to framebuffer, runs in browser via wasm.",
 		year: "2025",
 		status: "In Progress",
 		statusIcon: "more_horiz",
@@ -89,7 +103,7 @@ export const projects: Project[] = [{
 		status: "Done",
 		statusIcon: "done",
 		repo: "https://github.com/jktham/euclid",
-		live: "",
+		live: undefined,
 		tools: ["C++", "GLSL", "OpenGL"],
 		images: ["euclid/1.jpg", "euclid/2.jpg", "euclid/3.jpg", "euclid/4.jpg", "euclid/5.jpg", "euclid/6.jpg", "euclid/7.jpg"],
 		contributors: [{
@@ -103,7 +117,7 @@ export const projects: Project[] = [{
 		status: "Done",
 		statusIcon: "done",
 		repo: "https://github.com/jktham/blockgame",
-		live: "",
+		live: undefined,
 		tools: ["C++", "OpenGL"],
 		images: ["blockgame/1.jpg", "blockgame/2.jpg", "blockgame/3.jpg", "blockgame/4.jpg", "blockgame/5.jpg", ],
 		contributors: [{
@@ -117,7 +131,7 @@ export const projects: Project[] = [{
 		status: "In Progress",
 		statusIcon: "more_horiz",
 		repo: "https://github.com/jktham/rivulet",
-		live: "",
+		live: undefined,
 		tools: ["JavaScript", "React", "NextJS"],
 		images: ["rivulet2/1.jpg", "rivulet2/2.jpg"],
 		contributors: [{
@@ -137,7 +151,7 @@ export const projects: Project[] = [{
 		status: "Done",
 		statusIcon: "done",
 		repo: "https://github.com/jktham/decSat",
-		live: "",
+		live: undefined,
 		tools: ["Python", "PyQt5"],
 		images: ["decSat/1.jpg", "decSat/2.jpg", "decSat/3.jpg", "decSat/4.jpg", "decSat/5.jpg"],
 		contributors: [{
@@ -165,7 +179,7 @@ export const projects: Project[] = [{
 		status: "Done",
 		statusIcon: "done",
 		repo: "https://github.com/jktham/helios",
-		live: "",
+		live: undefined,
 		tools: ["C++", "OpenGL"],
 		images: ["helios/1.jpg", "helios/2.jpg", "helios/3.jpg", "helios/4.jpg"],
 		contributors: [{
@@ -179,7 +193,7 @@ export const projects: Project[] = [{
 		status: "Done",
 		statusIcon: "done",
 		repo: "https://github.com/jktham/faketorio",
-		live: "",
+		live: undefined,
 		tools: ["Java", "LWJGL"],
 		images: ["faketorio/1.jpg", "faketorio/2.jpg", "faketorio/3.jpg"],
 		contributors: [{
@@ -193,7 +207,7 @@ export const projects: Project[] = [{
 		status: "Done",
 		statusIcon: "done",
 		repo: "https://github.com/jktham/9-to-5-simulator",
-		live: "",
+		live: undefined,
 		tools: ["C#", "Unity", "Blender"],
 		images: ["9to5simulator/1.jpg", "9to5simulator/2.jpg", "9to5simulator/3.jpg"],
 		contributors: [{
@@ -210,7 +224,7 @@ export const projects: Project[] = [{
 		status: "In Progress",
 		statusIcon: "more_horiz",
 		repo: "https://github.com/jktham/spot",
-		live: "",
+		live: undefined,
 		tools: ["Rust", "crossterm"],
 		images: ["spot/1.jpg"],
 		contributors: [{
@@ -224,7 +238,7 @@ export const projects: Project[] = [{
 		status: "In Progress",
 		statusIcon: "more_horiz",
 		repo: "https://github.com/jktham/cgtr",
-		live: "",
+		live: undefined,
 		tools: ["C++", "C", "Teensy41"],
 		images: ["cgtr/1.jpg"],
 		contributors: [{
@@ -238,7 +252,7 @@ export const projects: Project[] = [{
 		status: "Done",
 		statusIcon: "done",
 		repo: "https://github.com/jktham/bapple",
-		live: "",
+		live: undefined,
 		tools: ["Verilog", "FPGA"],
 		images: ["bapple/1.jpg", "bapple/2.jpg"],
 		contributors: [{
@@ -255,7 +269,7 @@ export const projects: Project[] = [{
 		status: "Done",
 		statusIcon: "done",
 		repo: "https://github.com/jktham/mlib",
-		live: "",
+		live: undefined,
 		tools: ["Rust", "crossterm"],
 		images: ["mlib/1.jpg"],
 		contributors: [{
@@ -269,7 +283,7 @@ export const projects: Project[] = [{
 		status: "Done",
 		statusIcon: "done",
 		repo: "https://github.com/jktham/gregOS",
-		live: "",
+		live: undefined,
 		tools: ["Lua", "OpenComputers"],
 		images: ["gregOS/1.jpg", "gregOS/3.jpg", "gregOS/5.jpg"],
 		contributors: [{
